4bbebabcd36444fd7b3e193f5990c5bae9d9d132
Sean Duiser <duzer84@gmail.com>
2022-03-08 12:55:06 -0500

    can now store city and unique city attributes in local storage

--- script.js
+++ script.js
@@ -1,58 +1,77 @@
 var searchBtn = document.querySelector("#searchBtn");
 
-function getWeather () {
-    var latVar = localStorage.getItem('cityLat');
-    var lonVar = localStorage.getItem('cityLon');
-    console.log(latVar);
-    console.log(lonVar);
-    var requestURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latVar + '&lon=' + lonVar + '&appid=8a4f71946c01452c9735df61812f9851&units=imperial';
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        
-        var weatherEl = document.querySelector('#main-display');
-        
-        var cardEl = document.createElement('div');
-        var h3El = document.createElement('h3');
-        var descEl = document.createElement('p');
-        
-        h3El.textContent = localStorage.getItem('city');
-        descEl.textContent = data.current.temp;
-        
-        cardEl.appendChild(h3El);
-        cardEl.appendChild(descEl);
-        weatherEl.appendChild(cardEl);
-        
-        })
-};
-
-function searchMyCity () {
+function searchMyCity() {
     var myCity = document.getElementById("cityLookup").value;
-    localStorage.setItem('city', JSON.stringify(myCity));
+    console.log(myCity);
+    localStorage.setItem(JSON.stringify(myCity), JSON.stringify(myCity));
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + myCity + '&limit=5&appid=85935cb03b82ef5a71b61e85e479e53c&fo=json')
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            localStorage.setItem('cityLat', JSON.stringify(data[0].lat));
-            localStorage.setItem('cityLon', JSON.stringify(data[0].lon));
+            var latData = JSON.stringify(data[0].lat);
+            var lonData = JSON.stringify(data[0].lon);
+
+            localStorage.setItem(JSON.stringify(myCity) + '-cityLat', latData);
+            localStorage.setItem(JSON.stringify(myCity) + '-cityLon', lonData);
+
+            console.log(latData);
+            console.log(lonData);
+
+            var requestURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latData + '&lon=' + lonData + '&appid=8a4f71946c01452c9735df61812f9851&units=imperial';
+
+            fetch(requestURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+
+                    var weatherEl = document.querySelector('#main-display');
+
+                    var cardEl = document.createElement('div');
+                    var h3El = document.createElement('h3');
+                    var descEl = document.createElement('p');
+
+                    h3El.textContent = localStorage.getItem('city');
+                    descEl.textContent = data.current.temp;
+
+                    cardEl.appendChild(h3El);
+                    cardEl.appendChild(descEl);
+                    weatherEl.appendChild(cardEl);
+
+                })
 
         })
-     
-    getWeather();
+
 };
 
-function handleSubmission (event) {
-    event.preventDefault();
-    console.log('HERE');
-    searchMyCity();
-};
+// function getWeather() {
+//     fetch(requestURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
 
-searchBtn.addEventListener('click', handleSubmission);
+//             var weatherEl = document.querySelector('#main-display');
 
-// need to give lat/long localStorage keys unique names each time they're saved
+//             var cardEl = document.createElement('div');
+//             var h3El = document.createElement('h3');
+//             var descEl = document.createElement('p');
+
+//             h3El.textContent = localStorage.getItem('city');
+//             descEl.textContent = data.current.temp;
+
+//             cardEl.appendChild(h3El);
+//             cardEl.appendChild(descEl);
+//             weatherEl.appendChild(cardEl);
+
+//         })
+// };
+
+
+searchBtn.addEventListener('click', searchMyCity);
+
 // localStorage.key could be used to ensure "past search" buttons persist on page
 // a loop that runs on page load that iterates through all of they keys, and runs getWeather for each of them
