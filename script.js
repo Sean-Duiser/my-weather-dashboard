0ca3c6eab4da7ea3cf240919525041b96721a5c0
Sean Duiser <duzer84@gmail.com>
2022-03-08 10:36:57 -0500

    app now shows different data based on city user types

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var searchBtn = document.querySelector("#searchBtn");
 
 var searchMyCity = function () {
     var myCity = document.getElementById("cityLookup").value;
-    localStorage.setItem('city', JSON.stringify(myCity))
+    localStorage.setItem('city', JSON.stringify(myCity));
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + myCity + '&limit=5&appid=85935cb03b82ef5a71b61e85e479e53c&fo=json')
         .then(function (response) {
             return response.json();
@@ -14,40 +14,43 @@ var searchMyCity = function () {
             console.log(localStorage.getItem('cityLat'));
             console.log(localStorage.getItem('cityLon'));
         })
-        getWeather();
+    getWeather();
 };
 
-var latVar = localStorage.getItem('cityLat');
-var lonVar = localStorage.getItem('cityLon');
-var requestURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latVar + '&lon=' + lonVar + '&appid=8a4f71946c01452c9735df61812f9851&units=imperial';
-
-// currently the app will not fetch an updated requestURL when a 2nd set of lat/lon is acquired
+// currently the app requires an extra button click (check event listener function) to produce a 2nd result
 
 var getWeather = function () {
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+    var latVar = localStorage.getItem('cityLat');
+    var lonVar = localStorage.getItem('cityLon');
+    console.log(latVar);
+    console.log(lonVar);
+    var requestURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latVar + '&lon=' + lonVar + '&appid=8a4f71946c01452c9735df61812f9851&units=imperial';
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-        var weatherEl = document.querySelector('#main-display');
+            var weatherEl = document.querySelector('#main-display');
 
-        var cardEl = document.createElement('div');
-        var h3El = document.createElement('h3');
-        var descEl = document.createElement('p');
+            var cardEl = document.createElement('div');
+            var h3El = document.createElement('h3');
+            var descEl = document.createElement('p');
 
-        h3El.textContent = localStorage.getItem('city');
-        descEl.textContent = data.current.temp;
+            h3El.textContent = localStorage.getItem('city');
+            descEl.textContent = data.current.temp;
 
-        cardEl.appendChild(h3El);
-        cardEl.appendChild(descEl);
-        weatherEl.appendChild(cardEl);
+            cardEl.appendChild(h3El);
+            cardEl.appendChild(descEl);
+            weatherEl.appendChild(cardEl);
 
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+            localStorage.clear();
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 };
 
 searchBtn.addEventListener('click', searchMyCity);
